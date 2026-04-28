7e9852eb54a1f36c0a13253c8e51223e3121f7aa
Yehuda Wubaye <alemusee@gmail.com>
2026-04-28 17:16:30 +0300

    update javascript

---
+++ javaScript/exercises/exercises.js
@@ -0,0 +1,67 @@
+// console.log(10);
+// console.log("Hello world");
+
+// console.log(10 + 10);
+// console.log(10 - 5);
+// console.log(10 / 2);
+// console.log(10 * 2);
+
+// console.log(10 % 3);
+// console.log(10 ** 2); // 10 to the power of 2
+
+// let x = 10;
+// x += 5;
+// console.log(x);
+// console.log("hello " + "world");
+
+// // variables
+// let name = "Yehuda";
+// console.log("Hello " + name);
+// let age = 30;
+// console.log("I am " + age + " years old");
+
+// constants
+// const birthYear = 1991;
+// console.log("My birth year is " + birthYear);
+// // birthYear = 1991; // This will cause an error because birthYear is a constant
+
+// exercise
+// let firstName = "Yehuda";
+// let age = 30;
+// const country = "Israel";
+
+// // backticks and template literals
+// console.log(
+//   `My name is ${firstName}, I am ${age} years old and I live in ${country}.`,
+// );
+// console.log(`In 5 years, I will be ${age + 5} years old.`);
+
+// if conditions
+// if(condition) {
+
+// }
+// console.log(10 > 8);
+// console.log(10 < 8);
+
+// let age = 30;
+// if (age > 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are a minor.");
+
+//   let age = 30;
+//   let haTicke = true;
+//   if (age > 18 && haTicke === true) {
+//     console.log("You are an adult and have a ticket.");
+//   }
+// }
+// const randomnumber = Math.random();
+// if (randomnumber >= 0 && randomnumber < 1 / 3) {
+//   console.log("Greater than or equal to 0");
+// }
+// = assignment operator
+// == equality operator (loose equality)
+// === strict equality operator
+
+// console.log(10 == "10"); // true
+// console.log(10 === "10"); // false
