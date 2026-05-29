48df35c598e928ed1dccc3215441d796a55f62aa
alemusee-cmd <alemusee@gmail.com>
2026-05-29 11:24:58 +0000

    update

--- countries-api/script.js
+++ countries-api/script.js
@@ -9,11 +9,40 @@ let AllCuntries = [];
 
 async function getAllCountries() {
   const response = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,capital,currencies,flag,population,symbol,languages",
+    "https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,population,symbol,languages",
   );
   const countriesData = await response.json();
 
   console.log(countriesData);
+  displyCountries(countriesData);
+}
+
+function displyCountries(countries) {
+  countriesContainer.innerHTML = "";
+  countries.forEach((country) => {
+    const languagesArray = country.languages
+      ? Object.values(country.languages)
+      : [];
+    const textToShow = languagesArray.join(", ") || "אין מידע על מטבעות";
+    const currenciesArray = country.currencies
+      ? Object.values(country.currencies)
+      : [];
+    const textToShowCurrencies =
+      currenciesArray
+        .map((curr) => `${curr.name} (${curr.symbol || ""})`)
+        .join(", ") || "אין מידע על מטבע";
+    countriesContainer.innerHTML += `
+      <div class ="country-card">
+      <img src="${country.flags.png}"/>
+        <div class = "country-info">
+        <p><strong>name: </strong>${country.name.common}</p>
+        <p><strong>capital: </strong>${country.capital}</p>
+        <p><strong>language: </strong> ${textToShow}</p>        
+        <p><strong>currencies: </strong>${textToShowCurrencies}</p>
+        </div>
+      </div>
+      `;
+  });
 }
 
 getAllCountries();
--- test/javascript.js
+++ test/javascript.js
@@ -1,7 +1,145 @@
-const checkButoon = document.querySelector(".checkBtn");
-const inputField = document.querySelector(".userAnswer");
-const massage = document.querySelector(".resultMassage");
+// let name = "Yehuda"; //string
+// let age = 35; //number
+// let isActive = true; //boolean
+// let some = undefined; //undefined
+// let userSelect = null;
 
-checkButoon.addEventListener("click", () => {
-  const userAnswer = Number(inputField.value);
-});
+// console.log(20 + 20);
+// let x = 20;
+
+// console.log(x + 20);
+// let fullName = "Yehuda";
+// console.log("my name is " + fullName);
+
+// let name = "Yehuda";
+// let age = 40;
+// const country = "israel";
+// console.log(`my name is ${name}, ${age} years old and i live in ${country}`);
+
+// let age = 20;
+// if (age >= 18) {
+//   console.log("sucess");
+// } else {
+//   console.log("defiend");
+// }
+// function name() {
+//   let firstName = "Yehuda";
+//   console.log(firstName);
+// }
+// name();
+
+// function givenumber() {
+//   return 5;
+// }
+// const result = givenumber();
+// console.log(result);
+
+// var y = 4;
+// if (y == 4) {
+//   var y = 2;
+//   console.log(y);
+// }
+
+// function getNumber(x, y) {
+//   if (x > 0 && y > 0) {
+//     return x * y;
+//   }
+//   return 0;
+// }
+// console.log(getNumber(5, 8));
+// console.log(getNumber(-9, 8));
+
+// let str = "";
+// for (let i = 0; i < 100; i++) {
+//   str += i;
+// }
+// console.log(str);
+
+// for (let i = 0; i < 9; i++) {
+//   console.log(i);
+// }
+
+// for (let i = ("start" in window) ? window.start : 0; i < 9; i++) {
+//   console.log(i);
+// }
+
+// let i = 4;
+// for (; i < 9; i++) {
+//   console.log(i);
+//   // more statements
+// }
+
+// for (let i = 0; ; i++) {
+//   console.log(i);
+//   if (i > 10) break;
+//   // more statements
+// }
+
+// let x = 80;
+// if (x === 80) {
+//   let x = 100;
+//   console.log(x);
+// }
+// console.log(x);
+
+// function updateBrand(obj) {
+//   obj.brand = "Toyota";
+//   obj = null;
+// }
+// const car = {
+//   brand: "Honda",
+//   model: "Accord",
+//   year: 1998,
+// };
+// console.log(car.brand);
+// updateBrand(car);
+// console.log(car.brand);
+// console.log(car.model);
+// console.log(car.year);
+// console.log(car.brand + car.model + car.year);
+
+// let selectedColor = "green";
+// switch (selectedColor) {
+//   case "red":
+//     console.log("stop");
+//     break;
+//   case "yellow":
+//     console.log("prepare to drive");
+//     break;
+//   case "green":
+//     console.log("start drive");
+//     break;
+//   default:
+//     console.log("error");
+// }
+
+// while
+// let i = 0;
+// while (i < 10) {
+//   console.log("Hello" + i);
+
+//   i = i + 1;
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log("Hello" + (i + 1));
+
+//   //   i += 1;
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   console.log("Hello" + (i + 1));
+//   i++;
+// } while (i < 10);
+
+// for (let i = 0; 1 < 10; i++) {
+//   console.log("Hello" + (i + 1));
+// }
+
+let fruits = ["banana", "mango", "orange", "apple"];
+for (let i = 0; 0 < fruits.length; i++) {
+  console.log(fruits[i]);
+}
