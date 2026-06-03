e5afa5c7b4520ff7b1b149d35ab99833cfe340f6
alemusee-cmd <alemusee@gmail.com>
2026-06-03 19:20:43 +0000

    update

--- exercise/javascript.js
+++ exercise/javascript.js
@@ -171,16 +171,135 @@
 
 // ----------------------------------------------------------
 
-const vat = 0.18;
-let price = 300;
+// // calculate_vat
+// const vat = 0.18;
+// let price = 300;
 
-function calculateTotal(vat, price) {
-  if (vat != 0.18) {
-    return "error: vat amount's incorrect";
-  } else {
-    let taxAmount = price * vat;
-    return price + taxAmount;
-  }
-}
-let sucessResult = calculateTotal(vat, price);
-console.log(sucessResult);
+// function calculateTotal(vat, price) {
+//   if (vat != 0.18) {
+//     return "error: vat amount's incorrect";
+//   } else {
+//     let taxAmount = price * vat;
+//     return price + taxAmount;
+//   }
+// }
+// let sucessResult = calculateTotal(vat, price);
+// console.log(sucessResult);
+
+// // checkpassword
+// let mypassword = "124sjh6358";
+// function checkPassword(password) {
+//   if (password.length < 8) {
+//     return "error. paosswrd is short";
+//   } else {
+//     return `your password ${password} success`;
+//   }
+// }
+// let result = checkPassword(mypassword);
+// console.log(result);
+
+// checkServerLoad
+// let currentUsers = 99999;
+// const maxCapacity = 10000;
+// function checkServerLoad(users, limit) {
+//   if (users > limit) {
+//     return "סכנה: השרת קרס!";
+//   } else if (users === limit) {
+//     return "אזהרה: השרת מלא לחלוטין";
+//   } else {
+//     return "השרת במצב תקין";
+//   }
+// }
+// let result = checkServerLoad(currentUsers, maxCapacity);
+// console.log(result);
+
+// calculateShipping(cart)
+// let cartTotal = 500;
+// function calculateShipping(cart) {
+//   if (cart < 100) {
+//     return cart + 30;
+//   } else if (cart > 100 && cart <= 300) {
+//     return cart + 15;
+//   } else {
+//     return cart;
+//   }
+// }
+// let result = calculateShipping(cartTotal);
+// console.log(result);
+
+// -------------------------------------------------------
+
+// let serverList = ["ok", "ok", "ERROR", "ok"];
+// function scanServers(serverList) {
+//   for (let i = 0; i < statuses.length; i++) {
+//     if (serverList[i] === "ERROR") {
+//       return "סכנה: נמצא שרת תקול!";
+//     }
+//   }
+//   return "השרת נמצא תקין";
+// }
+
+// let result = scanServers(statuses);
+// console.log(result);
+
+// -----------------------------------------------------------
+// let cartPrices = [50, 120, 30];
+// function calculateCart(Prices) {
+//   let total = 0;
+//   for (let i = 0; i < Prices.length; i++) {
+//     total = total + Prices[i];
+//   }
+//   return `הסכום הכולל לתשלום הוא: ${total} ש"ח`;
+// }
+// let result = calculateCart(cartPrices);
+// console.log(result);
+
+// -------------------------------------------------------------
+// let serverLogs = [
+//   "INFO",
+//   "ERROR",
+//   "WARNING",
+//   "INFO",
+//   "ERROR",
+//   "ERROR",
+//   "ERROR",
+// ];
+// function countError(logs) {
+//   let errorCounts = 0;
+//   for (let i = 0; i < logs.length; i++) {
+//     if (logs[i] === "ERROR") {
+//       errorCounts++;
+//     }
+//   }
+//   return `there are ${errorCounts} errors`;
+// }
+// let result = countError(serverLogs);
+// console.log(result);
+
+// --------------------------------------------------------------
+// let cpuLoads = [45, 82, 12, 95, 60];
+// function findMaxLoad(loads) {
+//   let max = loads[0];
+//   for (let i = 0; i < loads.length; i++) {
+//     if (loads[i] > max) {
+//       max = loads[i];
+//     }
+//   }
+//   return `the max cpu is ${max}`;
+// }
+// let result = findMaxLoad(cpuLoads);
+// console.log(result);
+
+// --------------------------------------------------------------
+// let allServers = [12, 55, 30, 88, 10];
+// function filterLargeServers(servers) {
+//   let bigServers = [];
+//   for (let i = 0; i < servers.length; i++) {
+//     if (servers[i] > 50) {
+//       bigServers.push(servers[i]);
+//     }
+//   }
+//   return bigServers;
+// }
+// let result = filterLargeServers(allServers);
+// console.log(result);
